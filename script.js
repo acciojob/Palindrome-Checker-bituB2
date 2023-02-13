ce120002b300200f285c61ca57ea1574d3c36b95
Bitu Abraham <120098241+bituB2@users.noreply.github.com>
2023-02-13 22:43:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-	return str = str.split(' ').reverse().join(' ');
+	return str = str.split('').reverse().join('');
 
 }
-module.exports = palindrome
+module.exports = palindrome;
